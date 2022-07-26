2521b67d18e805d319dcb5f0596d7fb90d5f1f92
Josh <josh@Genesis.localdomain>
2022-07-26 01:17:47 +0100

    Laid out home page user interface

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home(){
   return(
@@ -7,7 +8,18 @@ export default function Home(){
       <title>Home Page</title>
     </Head>
     <main>
-      <h2 className='text-center'>Hello World!</h2>
+      <div className={utilStyles.homePageWrapper}>
+        <div className="p-5 md-col-6 container border bg-light text-center">
+          <h3 className='mb-3'>Enter chat room</h3>
+          <div className='input-group'>
+            <input type="text" placeholder='Room ID' className='form-control'></input>
+            <input type="text" placeholder='Nickname' className='form-control'></input>
+            <div className='input-group-append'>
+              <button className="btn btn-outline-primary" type="button" style={{borderTopLeftRadius: 0, borderBottomLeftRadius: 0, marginLeft: -1}}>Enter</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
     </>
   )
